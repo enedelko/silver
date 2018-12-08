0c1f284c3894a907c50ad6a807a0d3bf46db2a78
enedelko <evgeny.nedelko@gmail.com>
2018-12-09 02:09:18 +0300

    beautify + 2019 y support

--- afi.js
+++ afi.js
@@ -1,102 +1,112 @@
+/* jshint esversion:6,node:true */
 'use strict';
 
-const https=require('https');
+const https = require('https');
 const fs = require('fs');
 
 function logErr(err) {
-  console.error("Error: " + err.message);                                         
+  console.error("Error: " + err.message);
 }
 
 function loadImage(url, path) {
   if (!fs.existsSync(path)) {
     https.get(url, (resp) => {
         resp.setEncoding('binary');
-   
+
         let data = [];
         let firstChunk = true;
-        
+
         // A chunk of data has been recieved.                                         
         resp.on('data', (chunk) => {
           if (firstChunk) {
-            console.log('loading ' + url);     
+            console.log('loading ' + url);
             firstChunk = false;
           }
           data.push(Buffer.from(chunk, 'binary'));
-        });                                                                           
-   
+        });
+
         // The whole response has been received. Print out the result.                
         resp.on('end', () => {
           let ws = fs.createWriteStream(path);
-   
+
           ws.on('ready', () => {
             console.log('saving ' + url + ' to ' + path);
             ws.end(Buffer.concat(data));
           });
-          ws.on("error", logErr) 
+          ws.on("error", logErr);
         });
-    })
-    .on("error", logErr)
+      })
+      .on("error", logErr);
   }
 }
 
 function loadMonth(dataId) {
-  let url = 'https://afiserebryakova.ru/ajax/construction_load?id='+dataId;
+  let url = 'https://afiserebryakova.ru/ajax/construction_load?id=' + dataId;
   https.get(url, (resp) => {
       let data = [];
       let firstChunk = true;
-      
+
       // A chunk of data has been recieved.                                         
       resp.on('data', (chunk) => {
         if (firstChunk) {
-          console.log('loading ' + url);     
+          console.log('loading ' + url);
           firstChunk = false;
         }
         data.push(chunk);
-      });                                                                           
+      });
 
       // The whole response has been received. Print out the result.                
-      resp.on('end', () => {                                                        
+      resp.on('end', () => {
         let firstImage = true;
         JSON.parse(data.join('')).images.forEach((i) => {
           let p = i.src.indexOf('/201');
           if (firstImage) {
-            fs.mkdirSync(i.src.substring(p+1,p+11), { recursive: true });
+            fs.mkdirSync(i.src.substring(p + 1, p + 11), {
+              recursive: true
+            });
             firstImage = false;
           }
-          loadImage('https://afiserebryakova.ru' + encodeURIComponent(i.src).replace(/%2F/gi, '/'), i.src.substring(p+1,p+11) + '/' + i.file.replace(/\//g, '-') + '');
-        })
+          loadImage('https://afiserebryakova.ru' + encodeURIComponent(i.src).replace(/%2F/gi, '/'), i.src.substring(p + 1, p + 11) + '/' + i.file.replace(/\//g, '-') + '');
+        });
       });
-  })
-  .on("error", logErr)                                                                                
+    })
+    .on("error", logErr);
 }
 
 function loadYear(year) {
-  let url = 'https://afiserebryakova.ru/about/construction/'+year;
-  https.get(                                                                        
-    url,
-    {headers: {"X-PJAX":true}},                                                     
-    (resp) => {                                                                     
-      let data = '';                                                                
-      let firstChunk = true;
-      
-      // A chunk of data has been recieved.                                         
-      resp.on('data', (chunk) => {
-        if (firstChunk) {
-          console.log('loading ' + url);     
-          firstChunk = false;
+  let url = 'https://afiserebryakova.ru/about/construction/' + year;
+  https.get(
+      url, {
+        headers: {
+          "X-PJAX": true
         }
-        data += chunk;                                                              
-      });                                                                           
-                                                                                    
-      // The whole response has been received. Print out the result.                
-      resp.on('end', () => {                                                        
-        data.match(/data-id=\".*?\"/gi).forEach( (m) => {                               
-          loadMonth(m.replace(/data-id=\"(.*?)\"/, '$1'))
-        });                                                                         
-      });
-    }
-  )
-  .on("error", logErr)                                                                                
+      },
+      (resp) => {
+        let data = '';
+        let firstChunk = true;
+
+        // A chunk of data has been recieved.                                         
+        resp.on('data', (chunk) => {
+          if (firstChunk) {
+            console.log('loading ' + url);
+            firstChunk = false;
+          }
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.                
+        resp.on('end', () => {
+          const matches = data.match(/data-id=\".*?\"/gi);
+          if (matches) {
+            for (let m of matches) {
+              loadMonth(m.replace(/data-id=\"(.*?)\"/, '$1'));
+            }
+          }
+        });
+      }
+    )
+    .on("error", logErr);
 }
-loadYear(2017)
-loadYear(2018)
+loadYear(2017);
+loadYear(2018);
+loadYear(2019);
